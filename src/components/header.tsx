fe91d253bc4772492ad318adff645f3301313920
João Borchoski <128744140+JoaoBorchoski@users.noreply.github.com>
2023-06-06 15:47:32 -0300

    fix: responsive header

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,16 +1,28 @@
-import React from "react";
 import { useState } from "react";
+import { AiOutlineMenu } from "react-icons/ai";
 
-export const Header = (): any => {
+export const Header = (): JSX.Element => {
+    const { innerWidth } = window;
     const [isLogged, setIsLogged] = useState(false);
+    const [screenSize, setScreenSize] = useState(innerWidth);
+
+    window.addEventListener("resize", () => {
+        setScreenSize(window.innerWidth);
+    });
+
     return (
-        <header className="flex px-12 border-b-2 border-gray-300 font-medium ">
-            <section className="flex gap-2 items-baseline border-r-2 border-gray-300 w-5/6">
+        <header className="flex px-3 md:px-12 border-b-2 border-gray-300 font-medium items-center justify-between">
+            <section className="flex gap-2 items-baseline xl:w-5/6 w-7/12 md:border-r-2 md:border-gray-300 ">
                 <h1>Motor</h1>
                 <h2 className="text-2xl text-brand1">shop</h2>
             </section>
-            {isLogged == true ? (
-                <section className="flex items-center justify-center pl-8 w-1/6">
+
+            {screenSize < 768 ? (
+                <button>
+                    <AiOutlineMenu size={28} />
+                </button>
+            ) : isLogged == true ? (
+                <section className="flex items-center justify-center pl-8 w-5/12 xl:w-1/6 gap-4">
                     <p
                         className="bg-blue-500 p-3 rounded-full text-white"
                         onClick={() => setIsLogged(!isLogged)}
@@ -20,7 +32,7 @@ export const Header = (): any => {
                     <p className="p-3 ">Samuel Leão</p>
                 </section>
             ) : (
-                <nav className="flex items-center justify-between pl-8 w-1/6 ">
+                <nav className="flex items-center justify-between pl-8 w-5/12 xl:w-1/6 ">
                     <button
                         className="p-2.5 text-gray-500 "
                         onClick={() => setIsLogged(!isLogged)}
