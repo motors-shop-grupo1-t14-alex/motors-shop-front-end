b9d9537ca2a971a090b3bcc775a8b753d850a313
user <biancavell@hotmail.com>
2023-06-15 16:25:07 -0300

    feat: created customizable form.

--- src/components/input/index.tsx
+++ src/components/input/index.tsx
@@ -1,26 +1,10 @@
 import { iInput } from "./types";
 
-export const Input = ({
-    children,
-    placeHolder,
-    defaultValue,
-    id,
-    css,
-    inputCSS,
-    register,
-    type,
-}: iInput) => {
-    return (
-        <section className={`${css} flex flex-col`}>
-            <label htmlFor={id}>{children}</label>
-            <input
-                className={inputCSS}
-                id={id}
-                placeholder={placeHolder}
-                defaultValue={defaultValue}
-                type={type}
-                {...register}
-            />
-        </section>
-    );
+export const Input = ({ children, placeHolder, defaultValue, id, css, inputCSS, register, type }: iInput) => {
+  return (
+    <section className={`${css} flex flex-col`}>
+      <label htmlFor={id}>{children}</label>
+      <input className={inputCSS} id={id} placeholder={placeHolder} defaultValue={defaultValue} type={type} {...register} />
+    </section>
+  );
 };
--- src/components/input/types.ts
+++ src/components/input/types.ts
@@ -1,12 +1,12 @@
 import { UseFormRegisterReturn } from "react-hook-form";
 
 export interface iInput {
-    children: React.ReactNode;
-    placeHolder: string;
-    defaultValue?: string;
-    id: string;
-    css: string;
-    inputCSS: string;
-    register?: UseFormRegisterReturn<string>;
-    type: string;
+  children: React.ReactNode;
+  placeHolder: string;
+  defaultValue?: string;
+  id: string;
+  css: string;
+  inputCSS: string;
+  register?: UseFormRegisterReturn<string>;
+  type: string;
 }
