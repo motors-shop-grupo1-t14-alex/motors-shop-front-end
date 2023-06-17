9528e49e54b616d1cf5dd3368351a1f6f40c94ab
davi <daavisouza.santana@gmail.com>
2023-06-17 16:15:08 -0300

    fix: fixing protected routes

--- src/components/protectedRoutes/index.tsx
+++ src/components/protectedRoutes/index.tsx
@@ -1,15 +1,39 @@
 import { useContext, useEffect } from "react";
 import { Outlet, useNavigate } from "react-router-dom";
 import { UserContext } from "../../contexts/userContext/UserContext";
+import { api } from "../../services/axios";
 
 export const ProtectedRouted = () => {
   const { user } = useContext(UserContext);
   const navigate = useNavigate();
 
   useEffect(() => {
-    if (!user) {
+
+    const myId: string | null = localStorage.getItem("@ID");
+    const tokenString = localStorage.getItem("@TOKEN");
+
+    if (!tokenString) {
+      localStorage.clear();
       navigate("/login");
+
+      return
     }
+
+    const token = JSON.parse(tokenString)
+
+    const checkIfUserIsValid = async () => {
+      try {
+        api.defaults.headers.common.authorization = `Bearer ${token}`;
+        const teste = await api.get(`/users/${myId}`)
+        console.log(teste)
+      } catch {
+        localStorage.clear();
+        navigate("/login");
+      }
+    }
+    
+    checkIfUserIsValid()
+    
   }, []);
 
   return <>{user && <Outlet />}</>;
