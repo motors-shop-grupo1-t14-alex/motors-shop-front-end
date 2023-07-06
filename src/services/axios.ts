bccb274e5c5759fd19c5809b69b10cfcf3ee28f7
Davi Santana <105315342+DaviSouzaS@users.noreply.github.com>
2023-07-06 02:52:48 -0300

    Update axios.ts

--- src/services/axios.ts
+++ src/services/axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const api = axios.create({
-    baseURL: "http://localhost:3000/",
+    baseURL: "https://motors-shop-api-58os.onrender.com/",
 });
 
 export const fipe_api = axios.create({
